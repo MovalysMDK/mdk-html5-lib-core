703bc3253850bf8311b804420b2fc90ec186c539
Laurent <laurent.bonzom@soprasteria.com>
2016-05-03 08:46:25 +0200

    [MDK-1618][HTML5] KARMA conf

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,50 +1,68 @@
-module.exports = function(config) {
-  config.set({
-    browsers: ['Chrome'],
-    
-    frameworks: ['jasmine'],
-    
-    basePath : '../',
-    
-    files: [
-      /* CORE libs */
-      'vendor/angular/angular.js',
-      'vendor/angular-mocks/angular-mocks.js',
-      'vendor/angular-loading-bar/build/loading-bar.js',
-      'vendor/angular-translate/angular-translate.js',
-      'vendor/angular-resource/angular-resource.js',
-      'vendor/jquery/dist/jquery.js', 
-      'vendor/jasmine-jquery/lib/jasmine-jquery.js',
-      'vendor/modernizr/modernizr.js',
-      'lib/mfcoreModule.js',
-      /* MY EXPENSE DATA */
-      'test/unit/data/nosql/myexpense/mappings/moduleImport.js',
-      'test/unit/data/nosql/myexpense/factories/*.js',
-      'test/unit/data/nosql/myexpense/mappings/*.js',
-      'test/unit/data/nosql/myexpense/converters/*.js',
-      'test/unit/data/nosql/myexpense/dao/*.js',
-      'test/unit/data/nosql/myexpense/model/*.js',
-      'test/unit/data/nosql/myexpense/model/cascades/*.js',
-      /* DAO TEST DATA */
-      'test/unit/data/nosql/daotest/mappings/moduleImport.js',
-      'test/unit/data/nosql/daotest/factories/*.js',
-      'test/unit/data/nosql/daotest/mappings/*.js',
-      'test/unit/data/nosql/daotest/converters/*.js',
-      'test/unit/data/nosql/daotest/dao/*.js',
-      'test/unit/data/nosql/daotest/model/*.js',
-      'test/unit/data/nosql/daotest/model/cascades/*.js',
-      /* COMMON */
-      'lib/**/*.js',
-      'test/**/*.spec.js',
-      'test/unit/tools.js',
-      // assets
-      { pattern: 'test/unit/data/**/*.json', included: false, served: true },
-    ],
+module.exports = function (config) {
+    config.set({
+        browsers: ['Chrome'],
 
-    plugins : [
+        frameworks: ['jasmine'],
+
+        basePath: '../',
+
+        files: [
+            /* CORE libs */
+            'vendor/angular/angular.js',
+            'vendor/angular-mocks/angular-mocks.js',
+            'vendor/angular-loading-bar/build/loading-bar.js',
+            'vendor/angular-translate/angular-translate.js',
+            'vendor/angular-resource/angular-resource.js',
+            'vendor/jquery/dist/jquery.js',
+            'vendor/jasmine-jquery/lib/jasmine-jquery.js',
+            'vendor/modernizr/modernizr.js',
+            'lib/mfcoreModule.js',
+            /* MY EXPENSE DATA NOSQL */
+            /*'test/unit/data/nosql/myexpense/mappings/moduleImport.js',
+             'test/unit/data/nosql/myexpense/factories/!*.js',
+             'test/unit/data/nosql/myexpense/mappings/!*.js',
+             'test/unit/data/nosql/myexpense/converters/!*.js',
+             'test/unit/data/nosql/myexpense/dao/!*.js',
+             'test/unit/data/nosql/myexpense/model/!*.js',
+             'test/unit/data/nosql/myexpense/model/cascades/!*.js',*/
+            /* DAO TEST DATA NOSQL */
+            /* 'test/unit/data/nosql/daotest/mappings/moduleImport.js',
+             'test/unit/data/nosql/daotest/factories/!*.js',
+             'test/unit/data/nosql/daotest/mappings/!*.js',
+             'test/unit/data/nosql/daotest/converters/!*.js',
+             'test/unit/data/nosql/daotest/dao/!*.js',
+             'test/unit/data/nosql/daotest/model/!*.js',
+             'test/unit/data/nosql/daotest/model/cascades/!*.js',*/
+            /* DAO TEST DATA SQL */
+            'test/unit/data/sql/daotest/Tools/constant.js',
+            'test/unit/data/sql/daotest/Tools/CreateUserTable.js',
+            'test/unit/data/sql/daotest/Tools/FillUserTable.js',
+            'test/unit/data/sql/daotest/Tools/TestService.js',
+            'test/unit/data/sql/daotest/factories/*.js',
+            'test/unit/data/sql/daotest/proxy/*.js',
+            'test/unit/data/sql/daotest/mappings/*.js',
+            'test/unit/data/sql/daotest/converters/*.js',
+            'test/unit/data/sql/daotest/dao/*.js',
+            'test/unit/data/sql/daotest/model/*.js',
+            'test/unit/data/sql/daotest/model/cascades/*.js',
+            /* COMMON */
+            'lib/**/*.js',
+            /*'test/!**!/!*.spec.js',*/
+            'test/unit/dal/sql/MFDalWebSql.spec.js',
+            'test/unit/dal/sql/1-N/*.spec.js',
+            'test/unit/dal/sql/N-1/*.spec.js',
+            'test/unit/dal/sql/1-1/*.spec.js',
+            //'test/unit/dal/sql/1-N/DAO-cascades-daotest-sql.update.1--N.spec.js',
+            'test/unit/tools.js'
+            // assets
+            /*{ pattern: 'test/unit/data/!**!/!*.json', included: false, served: true },*/
+            //{pattern: 'test/unit/data/**/*.sql', included: false, served: true}
+        ],
+
+        plugins: [
             'karma-chrome-launcher',
             'karma-jasmine'
-            ]
- 
-  });
+        ]
+
+    });
 };
